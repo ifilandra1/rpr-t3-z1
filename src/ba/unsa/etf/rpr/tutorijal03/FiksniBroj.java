10700cc119091b4b3de6b989240c3209f62d30dd
student <ifilandra1@etf.unsa.ba>
2018-10-30 18:31:05 +0100

    Uradjena Program klasa djelimicno, na bolji nacin implementiran enum Grad

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -2,9 +2,39 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public class FiksniBroj extends TelefonskiBroj {
 
-    enum Grad {BIHAC,ORASJE,TUZLA,ZENICA,GORAZDE,TRAVNIK,MOSTAR,SIROKIBRIJEG,SARAJEVO,LIVNO,BRCKO };
-     String broj;
-     Grad grad;
+    public enum Grad {
+        BIHAC("037"), ORASJE("031"), TUZLA("035"), ZENICA("032"), GORAZDE("038"), TRAVNIK("030"), MOSTAR("036"), SIROKIBRIJEG("039"), SARAJEVO("033"), LIVNO("034"), BRCKO("049");
+        private String poz;
+
+        Grad(String s) {
+            this.poz = s;
+        }
+
+        public static Grad fromPozivni(String s){
+            switch (s) {
+                case "037": return BIHAC;
+                case "031": return ORASJE;
+                case "035": return TUZLA;
+                case "032": return ZENICA;
+                case "038": return GORAZDE;
+                case "030": return TRAVNIK;
+                case "036": return MOSTAR;
+                case "039": return SIROKIBRIJEG;
+                case "034": return LIVNO;
+                case "049": return BRCKO;
+
+            }
+            return SARAJEVO;
+        }
+        public String getPoz() {
+            return this.poz;
+        }
+
+    }
+
+    ;
+    String broj;
+    Grad grad;
 
     public Grad getGrad() {
         return grad;
@@ -20,7 +50,7 @@ public class FiksniBroj extends TelefonskiBroj {
 
     public int compareTo(Object o) {
 
-        return this.getBroj().compareTo( ((FiksniBroj) o).getBroj()  );
+        return this.getBroj().compareTo(((FiksniBroj) o).getBroj());
 
     }
 
@@ -28,30 +58,25 @@ public class FiksniBroj extends TelefonskiBroj {
         this.grad = grad;
     }
 
-    public FiksniBroj(Grad grad, String broj){
-        this.grad=grad; this.broj=broj;
-    };
+    public FiksniBroj(Grad grad, String broj) {
+        this.grad = grad;
+        this.broj = broj;
+    }
 
-   public String ispisi(){
-       String s = new String();
-       if(grad==Grad.BIHAC)s+="037";
-       else if(grad==Grad.ORASJE)s+="031";
-       else if(grad==Grad.TUZLA)s+="035";
-       else if(grad==Grad.ZENICA)s+="032";
-       else if(grad==Grad.GORAZDE)s+="038";
-       else if(grad==Grad.TRAVNIK)s+="030";
-       else if(grad==Grad.MOSTAR)s+="036";
-       else if(grad==Grad.SIROKIBRIJEG)s+="039";
-       else if(grad==Grad.SARAJEVO)s+="033";
-       else if(grad==Grad.LIVNO)s+="034";
-       else if(grad==Grad.BRCKO)s+="049";
-       s+="/";
-       return s+this.broj;
-    };
+    ;
 
-   public int hashCode(){
+    public String ispisi() {
+        String s = new String();
+        s += this.grad.getPoz();
+        s += "/";
+        return s + this.broj;
+    }
 
-       return 2;
-   }
+    ;
+
+    public int hashCode() {
+
+        return 2;
+    }
 
 }
--- src/ba/unsa/etf/rpr/tutorijal03/Program.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Program.java
@@ -1,8 +1,49 @@
 package ba.unsa.etf.rpr.tutorijal03;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
+
+
+        Imenik imenik = new Imenik();
+        while(true) {
+
+            System.out.println("Unesite ime i prezime");
+            Scanner scan = new Scanner(System.in);
+            String ime = scan.next();
+            System.out.println("Koji tip broja zelite unijeti?");
+            System.out.println("1 - mobilni broj, 2- fiksni broj, 3- medjunarodni broj");
+            int n = scan.nextInt();
+            if (n == 1) {
+                System.out.println("Unesite pozivni broj za mobilnu mrezu");
+                int pozivni = scan.nextInt();
+                System.out.println("Unesite broj");
+                String broj = scan.next();
+                imenik.dodaj(ime, new MobilniBroj(pozivni, broj));
+            }
+            if (n == 2) {
+                System.out.println("Unesite pozivni za grad");
+                String pozivni = scan.next();
+                System.out.println("Unesite broj");
+                String broj = scan.next();
+                imenik.dodaj(ime, new FiksniBroj(FiksniBroj.Grad.fromPozivni(pozivni), broj));
+
+            }
+            if (n == 3) {
+                System.out.println("Unesite pozivni za drzavu");
+                String pozivni = scan.next();
+                System.out.println("Unesite broj");
+                String broj = scan.next();
+                imenik.dodaj(ime, new MedunarodniBroj(pozivni, broj));
+            }
+            System.out.println("Unesite 0 za kraj, 1 za nastavak");
+            n=scan.nextInt();
+            if(n==0)break;
+
+
+        }
+
 
 
 
